5fc7bb7bce7da9acc2a2296d3b800221200d0aae
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-11-01 19:20:37 +0200

    build login component

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Header from './components/Header';
 import Home from './components/Home';
 import Movie from './components/Movie';
 import Notfound from './components/Notfound';
-
+import Login from './components/Login';
 // CONTEXT
 import UserProvider from './context';
 
@@ -21,6 +21,7 @@ const App = () => (
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
         <Route path='/:movieId' element={<Movie />} />
         <Route path='/*' element={<Notfound />} />
       </Routes>
---
+++ src/components/Login.js
@@ -0,0 +1,73 @@
+import React, { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import API from '../API'
+
+// COMPONENTS
+import Button from './Button';
+
+// STYLES
+import { Wrapper } from './Login.styles';
+
+// CONTEXT
+import { Context } from '../context';
+
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(false);
+
+  const [_user, setUser] = useContext(Context);
+  const navigate = useNavigate();
+
+  const handleSubmit = async () => {
+    setError(false);
+    try {
+      const requestToken = await API.getRequestToken();
+      const sessionId = await API.authenticate(
+        requestToken,
+        username,
+        password
+      );
+
+      console.log('sessionId', sessionId)
+      setUser({ sessionId: sessionId.session_id, username });
+
+      navigate('/');
+    } catch (error) {
+      setError(true);
+    }
+  };
+
+  const handleInput = (e) => {
+    const name = e.currentTarget.name;
+    const value = e.currentTarget.value;
+
+    if (name === 'username') setUsername(value);
+    if (name === 'password') setPassword(value);
+  };
+
+  return (
+    <Wrapper>
+      {error && <div className='error'>There was an error!</div>}
+      <label>Username:</label>
+      <input
+        type='text'
+        value={username}
+        name='username'
+        onChange={handleInput}
+      />
+      <input
+        type='password'
+        value={password}
+        name='password'
+        onChange={handleInput}
+      />
+      <Button text='Login' calllback={handleSubmit} />
+
+    </Wrapper>
+  )
+}
+
+export default Login
